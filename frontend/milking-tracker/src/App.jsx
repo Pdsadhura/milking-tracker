943bc9be14196d4625c37cf7ef7093d5cf3b5c06
Pdsadhura <dhimanparas23@gmail.com>
2025-08-23 23:16:29 +0530

    [new optimize image use for the background and add some overlay effect]

--- frontend/milking-tracker/src/App.jsx
+++ frontend/milking-tracker/src/App.jsx
@@ -2,23 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Session from "./pages/Session";
 import History from "./pages/History";
-import milkBg from "./assets/milk-background.png";
+import milkBg from "./assets/milk-background.jpg";
 
 export default function App() {
   return (
+    <>
+    
     <div
-      className="flex flex-col h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${milkBg})` }}
-    >
-      <Router>
-        <div className="flex-1 flex items-center justify-center">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/session" element={<Session />} />
-            <Route path="/history" element={<History />} />
-          </Routes>
-        </div>
-      </Router>
+  className="relative flex flex-col h-screen bg-cover bg-center"
+  style={{ backgroundImage: `url(${milkBg})` }}
+>
+  
+  <div className="absolute inset-0 bg-black/40" />
+
+  <Router>
+    <div className="relative z-10 flex-1 flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/session" element={<Session />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
+  </Router>
+</div>
+</>
   );
 }
